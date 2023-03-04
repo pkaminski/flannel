d0a64b125f017c5052a8567d2d03cc00f359972b
Piotr Kaminski <piotr@ideanest.com>
2023-03-03 16:35:24 -0800

    Add file for repro.

---
+++ simple.go
@@ -0,0 +1,12 @@
+// upsertRootTaskMetadata upserts the root task metadata into the table given root task id.
+func upsertRootTaskMetadata(db orm.DB, rootTaskMetadata RootTaskMetadata) error {
+	_, err := db.Model(&rootTaskMetadata).
+		OnConflict("(root_task_id) DO UPDATE").
+		Set("platform_trace_uuids = EXCLUDED.platform_trace_uuids").
+		Set("platform_trace_data = EXCLUDED.platform_trace_data").
+		Set("platform_trace_custom_data = EXCLUDED.platform_trace_custom_data").
+		Set("data source = EXCLUDED.data_source").
+		Set("platform_trace_data = EXCLUDED.platform_trace_data").
+		Set("platform_trace_data = EXCLUDED.platform_trace_data").
+		Set("platform_trace_data = EXCLUDED.platform_trace_data");
+}
